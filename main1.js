3fb456a6f93067cb0640aeea21ca2f4ce965a275
Brie <76833153+BriePowell@users.noreply.github.com>
2021-10-25 11:08:13 -0400

    updated main.js

--- main1.js
+++ main1.js
@@ -1,7 +1,4 @@
-//create a field to intake location data
-function buttonClicked(){
-    //submit the user input and call required data
-}
+//create a field to intake location data (index1.html)
 
 //Weather info API
 const form = document.getElementById('weatherLocationForm')
@@ -13,7 +10,7 @@ form.addEventListener('submit', (e) => {
 })
 
 const getWeather = async function(weather){
-    let response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${cityname},${statecode}&appid={APIKEY}`)
+    let response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${cityname},${statecode}&appid={ebf4c782db662e7c38c7ccddf83a3ef1}`)
     let data = await response.json()
     return data
 }
@@ -22,30 +19,35 @@ const weatherForm = document.getElementById('weatherForm');
 
 weatherForm.addEventListener('submit', async (e) =>{
     e.preventDefault();
-    let highTemp = e.target.weatherForm
+    let highTemp = e.target.cityName
     console.log(highTemp);
     let highTemp = await getWeather(highTemp)
     let labelHigh = document.createElement('h3');
-    labelHigh.innerHTML = `${highTemperature.city}`
+    labelHigh.innerHTML = `${highTemperature.cityName}`
 
-    let lowTemp = e.target.weatherForm
+    let lowTemp = e.target.cityName
     console.log(lowTemp);
     let lowTemp = await getWeather(lowTemp)
     let labelLow = document.createElement('h3');
-    labelLow.innerHTML = `${lowTemperature.city}`
+    labelLow.innerHTML = `${lowTemperature.cityName}`
 
-    let humidity = e.target.weatherForm
+    let humidity = e.target.cityName
     console.log(humidity);
     let humidity = await getWeather(humidity)
     let labelHum = document.createElement('h3');
-    labelHum.innerHTML = `${humidity.city}`
+    labelHum.innerHTML = `${humidity.cityName}`
 
-    let forecast = e.target.weatherForm
+    let forecast = e.target.cityName
     console.log(forecast);
     let forecast = await getWeather(forecast)
     let labelFore = document.createElement('h3');
-    labelFore.innerHTML = `${forecast.city}`
+    labelFore.innerHTML = `${forecast.cityName}`
       
+
+    document.body.append(labelHigh);
+    document.body.append(labelLow);
+    document.body.append(labelHum);
+    document.body.append(labelFore);
 
 
 })
@@ -54,5 +56,3 @@ let btn = document.getElementById('btn')
 btn.addEventListener('click', /* => */ {})
 
 //create display fields for the output
-
-//call up weather data
